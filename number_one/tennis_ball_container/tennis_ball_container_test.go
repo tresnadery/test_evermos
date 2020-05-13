ae96d4d060acea240cce86fadea98265c44bbdc4
Clapping ape <deritresna@clappingape.com>
2020-05-14 03:30:16 +0700

    Add unit test for unverified container

--- number_one/tennis_ball_container/tennis_ball_container_test.go
+++ number_one/tennis_ball_container/tennis_ball_container_test.go
@@ -21,3 +21,18 @@ func TestVerifiedContainer(t *testing.T){
 	assert.Equal(t, fmt.Sprintf("Container number %d is full", 2), containers.Verified(1))
 	assert.True(t, reflect.DeepEqual(containers.array[1], []int{1, 1}))
 }
+
+func TestUnVerifiedContainer(t *testing.T){
+	containers := New()
+	for i := 0; i < 2; i++{
+		containers.AddContainer(2)
+	}
+	containers.Put(0)
+	containers.Put(1)
+	containers.Put(1)
+
+	assert.NotEqual(t, containers.IsFull(0), true)
+	assert.NotEqual(t, containers.Size(0), containers.Capacity(0))
+	assert.NotEqual(t, fmt.Sprintf("Container number %d is full", 2), containers.Verified(0))
+	assert.False(t, reflect.DeepEqual(containers.array[0], []int{1, 1}))
+}
