516a4b6c0de2f860b717fe098ba322925ef04c97
Clapping ape <deritresna@clappingape.com>
2020-05-14 06:57:42 +0700

    Add function main to run program

---
+++ number_two/main.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+	"fmt"
+	"github.com/tresnadery/test_evermos/number_two/queue"
+)
+func main(){
+	totalProduct := 5
+	queue := queue.New("jhon", "doe", "Audrey", "Clarissa")
+	queue.Enqueue("Maisey")
+	queue.Enqueue("Kirstin")
+
+	for i := totalProduct; i > 0; i--{
+		queue.Dequeue()
+		totalProduct--
+		if totalProduct <= 0 {
+			fmt.Println("Sold Out")
+			break
+		}
+	}
+	if queue.IsEmpty(){
+		fmt.Println("queue is clear")
+		return
+	}
+	fmt.Printf("Person on queue : %+v \n", queue.GetCustomers())
+}
