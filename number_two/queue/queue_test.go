21bb8d4a4fa74ccdf88d33302d268e002cbef637
Clapping ape <deritresna@clappingape.com>
2020-05-14 06:12:41 +0700

    Add unit testing for dequeue

--- number_two/queue/queue_test.go
+++ number_two/queue/queue_test.go
@@ -14,3 +14,25 @@ func TestEnqueue(t *testing.T){
 	assert.Equal(t, queue.Size(), 3)	
 	assert.True(t, reflect.DeepEqual(queue.GetCustomers(), []interface{}{"jhon", "doe", "alex"}))
 }
+
+func TestDequeue(t *testing.T){
+	queue := New("jhon", "doe")
+
+	customer, err := queue.Dequeue()
+	assert.Nil(t, err)
+	assert.Equal(t, queue.IsEmpty(), false)
+	assert.Equal(t, queue.Size(), 1)
+	assert.True(t, reflect.DeepEqual(queue.GetCustomers(), []interface{}{"doe"}))
+	assert.Equal(t, customer, "jhon")
+
+	customer, err = queue.Dequeue()
+	assert.Nil(t, err)
+	assert.Equal(t, queue.IsEmpty(), true)
+	assert.Equal(t, queue.Size(), 0)
+	assert.Equal(t, customer, "doe")
+
+	_, err = queue.Dequeue()
+	assert.NotNil(t, err)
+	assert.Equal(t, err.Error(), "queue is empty")
+
+}
