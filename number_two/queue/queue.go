2e6342df50b15a2f493cd902b31ed1a51cadadb2
Clapping ape <deritresna@clappingape.com>
2020-05-14 06:33:20 +0700

    Add Queue structure & Add factory to generate Queue

---
+++ number_two/queue/queue.go
@@ -0,0 +1,21 @@
+package queue
+
+import(
+	"errors"
+)
+var(
+	errQueueIsEmpty = errors.New("queue is empty")
+)
+// Queue Structure
+type Queue struct{
+	array []interface{}
+}
+
+// New factory to generate Queue
+func New(customers ...interface{})*Queue{
+	q := &Queue{
+		make([]interface{}, 0, len(customers)),
+	}
+	q.Enqueue(customers...)
+	return q
+}
